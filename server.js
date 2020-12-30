dc7e1cb1a4d923c1e40561ebcf08b500ad52cc92
nikki-ai <shony1198@gmail.com>
2020-12-29 20:13:28 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const movies = require('./MovieData.js');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN; 
+  const authToken = req.get('Authorization'); 
+  console.log('validate bearer token middleware');
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+
+  next();
+});
+
+function mainScreen(req, res) {
+  res.json('Moviedex-api! Go to /movies');
+}
+
+
+function handleGetMovie(req, res) {
+  const { genre, country, avg_vote } = req.query;
+  let results = movies;
+
+
+  if (genre){
+    results = movies.filter((m) => m.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
+
+  if (country){
+    results = movies.filter((m) => m.country.toLowerCase().includes(country.toLowerCase()));
+  }
+
+  if (avg_vote){
+    results = movies.filter((m) => Number(m.avg_vote) >= Number(avg_vote));
+  }
+
+  res.json(results);
+}
+
+app.get('/', mainScreen);
+app.get('/movie', handleGetMovie);
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
