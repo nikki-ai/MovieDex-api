37cf1caa2c3b17ced3419dcb82b0de2c807ffd8d
nikki-ai <shony1198@gmail.com>
2021-01-07 11:52:56 -0600

    heroku refactoring

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny': 'common';
 
-app.use(morgan('dev'));
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN; 
-  const authToken = req.get('Authorization'); 
-  console.log('validate bearer token middleware');
+  const authToken = req.get('Authorization');
 
   if(!authToken || authToken.split(' ')[1] !== apiToken){
     return res.status(401).json({error: 'Unauthorized request'});
@@ -50,8 +50,17 @@ function handleGetMovie(req, res) {
 app.get('/', mainScreen);
 app.get('/movie', handleGetMovie);
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
